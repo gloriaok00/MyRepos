92dd6df9c9a312b4ebf593ad6e896cae47fc7cce
zhangyu <zhangyu121.ok@163.com>
2020-11-30 15:25:56 +0800

    探索用字节数组查Mongo

--- demo/src/main/java/com/example/demo/pingshi/mongo/MongoBinanyDemo.java
+++ demo/src/main/java/com/example/demo/pingshi/mongo/MongoBinanyDemo.java
@@ -31,12 +31,16 @@ public class MongoBinanyDemo {
                 System.out.println(mongoCursor.next());
             }
             //Document searchById = new Document("_id", new Binary((byte) 3, Base64.getDecoder().decode("d0JdSkDbi7CnwA+E0zeQkw==")));
-            Document searchById = new Document("_id", new Binary((byte) 3, Base64.getDecoder().decode("d0JdSkDbi7CnwA+E0zeQkw==")));
+            byte[] bs= {119,66,93,74,64,-37,-117,-80,-89,-64,15,-124,-45,55,-112,-109};
+
+            Document searchById = new Document("_id", new Binary((byte) 3, bs));
             FindIterable<Document> findIterable1 = collection.find(searchById);
             MongoCursor<Document> mongoCursor1 = findIterable1.iterator();
             while(mongoCursor1.hasNext()){
                 System.out.println("结果:"+mongoCursor1.next());
             }
+
+            System.out.println("捋一下:"+Base64.getDecoder().decode("d0JdSkDbi7CnwA+E0zeQkw=="));
 
         }catch(Exception e){
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
