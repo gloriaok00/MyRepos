936c0c2ba7fb0a5c113bfdfb9d7b98d40896330d
zhangyu <zhangyu121.ok@163.com>
2020-11-30 20:57:38 +0800

    尝试用uuid查mongo binary 类型的id终于出来了

--- demo/src/main/java/com/example/demo/pingshi/mongo/MongoBinanyDemo.java
+++ demo/src/main/java/com/example/demo/pingshi/mongo/MongoBinanyDemo.java
@@ -6,13 +6,15 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.UuidRepresentation;
+import org.bson.internal.UuidHelper;
 import org.bson.types.Binary;
 
-import java.util.Base64;
+import java.util.UUID;
 
 /**
  * @author zhangyu
- * @description
+ * @description   mongo db 查询 binary类型 id
  * @date 2020-11-30 14:57
  */
 
@@ -30,17 +32,15 @@ public class MongoBinanyDemo {
             while(mongoCursor.hasNext()){
                 System.out.println(mongoCursor.next());
             }
-            //Document searchById = new Document("_id", new Binary((byte) 3, Base64.getDecoder().decode("d0JdSkDbi7CnwA+E0zeQkw==")));
-            byte[] bs= {119,66,93,74,64,-37,-117,-80,-89,-64,15,-124,-45,55,-112,-109};
 
-            Document searchById = new Document("_id", new Binary((byte) 3, bs));
+            //Document searchById = new Document("_id", new Binary((byte) 3, Base64.getDecoder().decode("009wZqTWYMB8aQPasIPquw==")));
+
+            Document searchById = new Document("_id", new Binary((byte) 3, UuidHelper.encodeUuidToBinary(UUID.fromString("4f218f5f-d749-4033-b970-54e21926a814"), UuidRepresentation.JAVA_LEGACY)));
             FindIterable<Document> findIterable1 = collection.find(searchById);
             MongoCursor<Document> mongoCursor1 = findIterable1.iterator();
             while(mongoCursor1.hasNext()){
                 System.out.println("结果:"+mongoCursor1.next());
             }
-
-            System.out.println("捋一下:"+Base64.getDecoder().decode("d0JdSkDbi7CnwA+E0zeQkw=="));
 
         }catch(Exception e){
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
