487ba944cfe851bfd7681d53a00c3fcf737e1c02
zhangyu <zhangyu121.ok@163.com>
2021-06-08 12:34:15 +0800

    mysql jdbc

---
+++ demo/src/main/java/com/example/demo/pingshi/jdbcMysql/MysqlJDBC.java
@@ -0,0 +1,70 @@
+package com.example.demo.pingshi.jdbcMysql;
+
+
+import java.sql.*;
+
+/**
+ * @author zhangyu
+ * @description
+ * @date 2021-06-08 11:29
+ */
+
+public class MysqlJDBC {
+
+    // MySQL 8.0 以下版本 - JDBC 驱动名及数据库 URL
+    //static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    //static final String DB_URL = "jdbc:mysql://localhost:3306/RUNOOB";
+
+    // MySQL 8.0 以上版本 - JDBC 驱动名及数据库 URL
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/PERSISTENCE_DB?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+
+    // 数据库的用户名与密码，需要根据自己的设置
+    static final String USER = "root";
+    static final String PASS = "!Qazxsw2";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            // 注册 JDBC 驱动
+            Class.forName(JDBC_DRIVER);
+
+            // 打开链接
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            // 执行查询
+            System.out.println(" 实例化Statement对象...");
+            stmt = conn.createStatement();
+
+            for (int i = 1; i < 21; i++) {
+                String sql = "INSERT INTO demo " +
+                        "VALUES ("+i+", 'dfdsf')";
+                stmt.executeUpdate(sql);
+            }
+
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        }catch(Exception e){
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        }finally{
+            // 关闭资源
+            try{
+                if(stmt!=null) stmt.close();
+            }catch(SQLException se2){
+            }// 什么都不做
+            try{
+                if(conn!=null) conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Goodbye!");
+    }
+}
