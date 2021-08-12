b9f521f0ab56e90dca638379b8908f391d0fbb4c
zhangyu <zhangyu121.ok@163.com>
2021-08-12 16:30:46 +0800

    siddhi source sink 中文本地先行试水

---
+++ demo/src/main/java/com/example/demo/pingshi/mongo/SiddhiZH.java
@@ -0,0 +1,45 @@
+package com.example.demo.pingshi.mongo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Aggregates;
+import org.bson.Document;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @author zhangyu
+ * @description
+ * @date 2021-08-12 14:52
+ */
+
+public class SiddhiZH {
+    public static void main(String[] args) throws JSONException {
+        // 连接到 mongodb 服务
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+        // 连接到数据库
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("Foo");
+        MongoCollection<Document> myCollect = mongoDatabase.getCollection("test2");
+        Document show=new Document();
+        show.put("_id",0);
+        show.put("type",0);
+        AggregateIterable<Document> result =myCollect.aggregate(
+                Arrays.asList(
+                        Aggregates.match(new Document("type", "source")),
+                        Aggregates.project(show)
+                )
+        );
+        System.out.println(result.into(new ArrayList<>()).size());
+        ArrayList<Document> dfsdf=result.into(new ArrayList<>());
+        JSONObject obj=new JSONObject();
+        obj.put("sources",dfsdf);
+        System.out.println("df");
+    }
+
+}
