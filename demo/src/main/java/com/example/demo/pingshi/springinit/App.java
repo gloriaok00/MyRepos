7e8b81d4beca9dee9c3fafc4f416808bb3e6eef5
zhangyu <zhangyu121.ok@163.com>
2021-10-22 16:56:28 +0800

    学习spring容器管理对象中

---
+++ demo/src/main/java/com/example/demo/pingshi/springinit/App.java
@@ -0,0 +1,20 @@
+package com.example.demo.pingshi.springinit;
+
+import com.example.demo.pingshi.springinit.sub.A;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author zhangyu
+ * @description
+ * @date 2021-10-22 16:36
+ */
+
+public class App {
+
+    public static void main(String[] args) {
+       ApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
+       A a= context.getBean(A.class);
+       a.show();
+    }
+}
