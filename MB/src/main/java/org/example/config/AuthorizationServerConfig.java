d6bf76a753f5def7b734aaa5ac620471100c1a7f
zhangyu <zhangyu121.ok@163.com>
2021-11-17 21:49:31 +0800

    MB与M1整合成功第一版

--- MB/src/main/java/org/example/config/AuthorizationServerConfig.java
+++ MB/src/main/java/org/example/config/AuthorizationServerConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -23,8 +25,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     AuthenticationManager authenticationManager;
 
     // 该对象用来将令牌信息存储到内存中
-    @Autowired(required = false)
-    TokenStore inMemoryTokenStore;
+   /* @Autowired(required = false)
+    TokenStore inMemoryTokenStore;*/
 
     // 该对象将为刷新token提供支持
     @Autowired
@@ -45,14 +47,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
             .withClient("password")
             .authorizedGrantTypes("password", "refresh_token") //授权模式为password和refresh_token两种
             .accessTokenValiditySeconds(1800) // 配置access_token的过期时间
-            .resourceIds("rid") //配置资源id
-            .scopes("all")
             .secret("$2a$10$RMuFXGQ5AtH4wOvkUqyvuecpqUSeoxZYqilXzbz50dceRsga.WYiq"); //123加密后的密码
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.tokenStore(inMemoryTokenStore) //配置令牌的存储（这里存放在内存中）
+
+        endpoints
+                .tokenStore(jwtTokenStore())
+                .accessTokenConverter(accessTokenConverter())
                 .authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService);
     }
@@ -62,4 +65,24 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         // 表示支持 client_id 和 client_secret 做登录认证
         security.allowFormAuthenticationForClients();
     }
+
+    /**
+     * 对Jwt签名时，增加一个密钥
+     * JwtAccessTokenConverter：对Jwt来进行编码以及解码的类
+     */
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("test-secret");
+        return converter;
+    }
+
+    /**
+     * 设置token 由Jwt产生，不使用默认的透明令牌
+     */
+    @Bean
+    public JwtTokenStore jwtTokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
 }
