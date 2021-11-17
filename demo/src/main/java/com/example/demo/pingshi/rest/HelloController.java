6678f9b3da6efb9fc0691f5e7467056c3bf412b9
zhangyu <zhangyu121.ok@163.com>
2021-11-17 10:43:20 +0800

    @RestController的理解

---
+++ demo/src/main/java/com/example/demo/pingshi/rest/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo.pingshi.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * @description  restcontroller本身这个参数是value，仅仅是
+ * 语义上的含义或说明 并不是路径的定义 若用路径的 需要用@RequestMapping
+ * @date 2019/12/1 15:13
+ */
+
+
+@RestController("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    //String name
+    public String say() {
+        return "hi , " + "name";
+    }
+
+}
