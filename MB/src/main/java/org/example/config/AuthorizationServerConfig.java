3307f8df9f8830b136bfd9cbd1b5ccab4b6f0578
zhangyu <zhangyu121.ok@163.com>
2021-11-18 20:26:31 +0800

    找到访问不了的原因

--- MB/src/main/java/org/example/config/AuthorizationServerConfig.java
+++ MB/src/main/java/org/example/config/AuthorizationServerConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -39,6 +38,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         return new BCryptPasswordEncoder();
     }
 
+    //curl -X POST --user 'password:123' -d 'grant_type=password&username=user&password=123' http://localhost:8088/oauth/token
     // 配置 password 授权模式
     @Override
     public void configure(ClientDetailsServiceConfigurer clients)
@@ -47,7 +47,18 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
             .withClient("password")
             .authorizedGrantTypes("password", "refresh_token") //授权模式为password和refresh_token两种
             .accessTokenValiditySeconds(1800) // 配置access_token的过期时间
-            .secret("$2a$10$RMuFXGQ5AtH4wOvkUqyvuecpqUSeoxZYqilXzbz50dceRsga.WYiq"); //123加密后的密码
+                .scopes("read")
+                .secret("$2a$10$RMuFXGQ5AtH4wOvkUqyvuecpqUSeoxZYqilXzbz50dceRsga.WYiq")//123加密后的密码
+            ;
+       /* clients.inMemory()
+                .withClient("app")
+                .secret("$2a$10$RMuFXGQ5AtH4wOvkUqyvuecpqUSeoxZYqilXzbz50dceRsga.WYiq")
+                .scopes("read")
+                //设置支持[密码模式、授权码模式、token刷新]
+                .authorizedGrantTypes(
+                        "password",
+                        "authorization_code",
+                        "refresh_token");*/
     }
 
     @Override
