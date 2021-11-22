9c1520a9b9c8eb008ac7d4e749ee2d3c02e30b4c
zhangyu <zhangyu121.ok@163.com>
2021-11-22 15:02:17 +0800

    spring security拦截规则学习中

--- MB/src/main/java/org/example/config/SecurityConfig.java
+++ MB/src/main/java/org/example/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.example.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,14 +12,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
      @Bean
      public UserDetailsService userDetailsService(){
          InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
          PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-         log.info("Password: {}", encoder.encode("123"));
-         manager.createUser(User.withUsername("dev").password(encoder.encode("123")).authorities("p1").build());
+         manager.createUser(User.withUsername("admin").password(encoder.encode("admin")).authorities("p1").roles("ADMIN").build());
+         manager.createUser(User.withUsername("user").password(encoder.encode("user")).authorities("p2").roles("USER").build());
          return manager;
      }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/home").hasRole("ADMIN");
+        http.authorizeRequests().antMatchers("/hello").hasRole("USER");
+        http.formLogin();
+    }
 }
