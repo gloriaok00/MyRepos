1f63a49d01855df0a6dc9114818e042fe3b1d6f5
zhangyu <zhangyu121.ok@163.com>
2021-11-23 10:10:13 +0800

    M1改版

--- M1/src/main/java/com/macro/cloud/Oauth2ServerApplication.java
+++ M1/src/main/java/com/macro/cloud/Oauth2ServerApplication.java
@@ -1,13 +1,13 @@
-package org.example;
+package com.macro.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class Oauth2ServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run( AppApplication.class, args );
+        SpringApplication.run(Oauth2ServerApplication.class, args);
     }
 
 }
