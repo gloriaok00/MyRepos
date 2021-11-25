9304868a5a649dd67291be35277d20e3ed24fb37
zhangyu <zhangyu121.ok@163.com>
2021-11-25 13:28:54 +0800

    补充提交资源服务器代码

---
+++ M2/src/main/java/org/example/rest/HiController.java
@@ -0,0 +1,16 @@
+package org.example.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HiController {
+
+    @GetMapping("/hi")
+    public String say() {
+        return "hi";
+    }
+
+}
