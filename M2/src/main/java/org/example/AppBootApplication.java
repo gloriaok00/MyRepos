aa76a5d00c90e73530e89861a207e24666faf4cd
zhangyu <zhangyu121.ok@163.com>
2021-11-25 10:20:49 +0800

    M2资源服务器init

---
+++ M2/src/main/java/org/example/AppBootApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AppBootApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(AppBootApplication.class,args);
+    }
+}
