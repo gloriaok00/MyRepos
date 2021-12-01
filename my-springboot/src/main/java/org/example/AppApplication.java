eff09e9910813437d9440341752d0103f113833e
zhangyu <zhangyu121.ok@163.com>
2021-12-01 13:35:35 +0800

    springboot项目脚手架，利用其制作了自己的springboot项目的maven archetype

---
+++ my-springboot/src/main/java/org/example/AppApplication.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description  springboot启动类
+ * @date 2021-12-01 08:56
+ */
+
+@SpringBootApplication
+public class AppApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(AppApplication.class,args);
+    }
+}
