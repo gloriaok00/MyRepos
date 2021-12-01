a1e9833efd1149d43e557d077de64b6731945bac
zhangyu <zhangyu121.ok@163.com>
2021-12-01 14:21:57 +0800

    1.移除无用测试项目 2.@componentScan扫描多个包，把自定义的其它jar里的component扫进来 3.整合starter-test类，移除其它无用的依赖

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,8 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+//"org.example"为mvndemo项目里的包路径，为了把里面的OtherJarBean扫进来
+//"com.example.demo"为demo工程的目录
+@ComponentScan({"org.example","com.example.demo"})
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @ServletComponentScan
 public class DemoApplication {
---
+++ demo/src/main/java/com/example/demo/pingshi/spring/myotherbean/MyLocalBean.java
@@ -0,0 +1,24 @@
+package com.example.demo.pingshi.spring.myotherbean;
+
+import org.example.config.OtherJarBean;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @description
+ * @date 2021-11-30 17:01
+ */
+
+@SpringBootTest
+public class MyLocalBean {
+
+    @Autowired
+    private OtherJarBean aaa;
+
+    @Test
+    public void show(){
+        System.out.println(aaa.a);
+    }
+
+}
