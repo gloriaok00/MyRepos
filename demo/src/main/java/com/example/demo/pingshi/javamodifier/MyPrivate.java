e48745d54bd4695eb6968380aec1c8f56b61eeab
zhangyu <zhangyu121.ok@163.com>
2021-12-02 14:19:12 +0800

    private修饰的东西只能在本类中使用，即使是子类也调不到

--- demo/src/main/java/com/example/demo/pingshi/javamodifier/MyPrivate.java
+++ demo/src/main/java/com/example/demo/pingshi/javamodifier/MyPrivate.java
@@ -4,7 +4,11 @@ package com.example.demo.pingshi.javamodifier;
  * @description
  * @date 2021-11-09 15:31
  */
+
  public class MyPrivate {
 
+     private int b=2;
      protected int a=1;
+     public String s="长点脑子，只要用了private修饰的变量，子类也用不了";
+
 }
