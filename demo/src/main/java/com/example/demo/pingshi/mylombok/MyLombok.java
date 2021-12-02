d1a8adb7d37cce822843420f30a83c4a38b7335b
zhangyu <zhangyu121.ok@163.com>
2021-12-02 11:23:14 +0800

    lombok @builder学习第二弹

--- demo/src/main/java/com/example/demo/pingshi/mylombok/MyLombok.java
+++ demo/src/main/java/com/example/demo/pingshi/mylombok/MyLombok.java
@@ -11,6 +11,7 @@ public class MyLombok {
 
     @Test
     public void initPerson(){
+
         Person p=new Person.Builder(12).id(1023).name("zy").age(34).build();
         System.out.println(p.getId());
         System.out.println(p.getName());
@@ -18,5 +19,10 @@ public class MyLombok {
 
         Student1 s1=Student1.builder().build();
         System.out.println(s1.getAge());
+
+        Person1 p1=Person1.builder().id(1).name("this is from lombok's name").build();
+        System.out.println(p1.getId());
+        System.out.println(p1.getName());
+
     }
 }
---
+++ demo/src/main/java/com/example/demo/pingshi/mylombok/Person1.java
@@ -0,0 +1,17 @@
+package com.example.demo.pingshi.mylombok;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * @description
+ * @date 2021-12-02 11:19
+ */
+
+@Builder
+@Getter
+public class Person1 {
+
+    private int id;
+    private String name;
+}
