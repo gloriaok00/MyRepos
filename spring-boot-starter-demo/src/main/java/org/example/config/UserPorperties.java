3a0c9e85cda5fd4329d62806e98df3b0a45976be
zhangyu <zhangyu121.ok@163.com>
2021-12-06 21:17:34 +0800

    自定义spring boot starter学习中..

---
+++ spring-boot-starter-demo/src/main/java/org/example/config/UserPorperties.java
@@ -0,0 +1,10 @@
+package org.example.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties("spring.user")
+public class UserPorperties {
+    private String name;
+}
