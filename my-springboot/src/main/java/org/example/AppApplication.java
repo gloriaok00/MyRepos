aaeb7eff61710a803de3ad1a74669b3efdcd23c4
zhangyu <zhangyu121.ok@163.com>
2021-12-06 22:01:50 +0800

    通过手动删除maven 骨架生成后的.idea文件方式，解决骨架里面含有不需要的文件问题

--- my-springboot/src/main/java/org/example/AppApplication.java
+++ my-springboot/src/main/java/org/example/AppApplication.java
@@ -3,10 +3,6 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @description  springboot启动类
- * @date 2021-12-01 08:56
- */
 
 @SpringBootApplication
 public class AppApplication
