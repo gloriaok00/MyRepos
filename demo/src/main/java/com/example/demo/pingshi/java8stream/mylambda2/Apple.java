b55d89d4cfa374ba1d4e0bce500d4a21d1c3c2e5
zhangyu <zhangyu121.ok@163.com>
2021-12-30 09:06:50 +0800

    lambda日常学习

---
+++ demo/src/main/java/com/example/demo/pingshi/java8stream/mylambda2/Apple.java
@@ -0,0 +1,17 @@
+package com.example.demo.pingshi.java8stream.mylambda2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @description
+ * @date 2021-12-30 01:42
+ */
+
+@Data
+@AllArgsConstructor
+public class Apple {
+
+    private Integer jin;
+    private Double price;
+}
