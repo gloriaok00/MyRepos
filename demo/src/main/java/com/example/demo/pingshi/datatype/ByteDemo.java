cb441f4d04ba297dbb37c05c41b5870780a1273b
zhangyu <zhangyu121.ok@163.com>
2022-01-05 15:05:38 +0800

    字节数

---
+++ demo/src/main/java/com/example/demo/pingshi/datatype/ByteDemo.java
@@ -0,0 +1,15 @@
+package com.example.demo.pingshi.datatype;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @description  字节数
+ * @date 2022-01-05 15:01
+ */
+
+public class ByteDemo {
+
+    public static void main(String[] args) {
+        System.out.println("te12我中".getBytes(StandardCharsets.UTF_8).length);
+    }
+}
