b590d144d9d4cbd25478b456c53c55ce1e1b5a42
zhangyu <zhangyu121.ok@163.com>
2022-02-05 22:10:17 +0800

    泛型类学习

---
+++ demo/src/main/java/com/example/demo/pingshi/datatype/GMain.java
@@ -0,0 +1,22 @@
+package com.example.demo.pingshi.datatype;
+
+import com.example.demo.pingshi.java8stream.Person;
+
+/**
+ * @description 泛型类学习
+ * @date 2022-02-05 22:05
+ */
+
+public class GMain {
+
+    public static void main(String[] args) {
+        Generic<Person> g1=new Generic<>(new Person("11",7));
+        System.out.println(g1.getKey().getAge());
+
+        Generic<String> g2=new Generic<>("dfdsf");
+        System.out.println(g2.getKey());
+
+        Generic g3=new Generic("ccc");
+        System.out.println(g3.getKey());
+    }
+}
