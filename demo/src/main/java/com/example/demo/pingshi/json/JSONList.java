106287abd45ba28506e057b1e6b48d56a011301f
zhangyu <zhangyu121.ok@163.com>
2022-02-23 20:17:19 +0800

    @requestparam再次学习

--- demo/src/main/java/com/example/demo/pingshi/json/JSONList.java
+++ demo/src/main/java/com/example/demo/pingshi/json/JSONList.java
@@ -1,24 +1,21 @@
 package com.example.demo.pingshi.json;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author zhangyu
  * @date 2019-10-28 21:08
  */
 
-@Controller
+@RestController
 @RequestMapping(value = "/dd")
 public class JSONList {
 
     //@RequestParam不加也行，
     // 不加时url里的参数名称与controller方法里的名称必须一致。加的话可以给value啥的换名称
     @GetMapping(value = "/json22")
-    public String getJson(String p1) {
-        System.out.println("pp:" + p1);
+    public String getJson(@RequestParam String name) {
+        System.out.println("pp:" + name);
         return "index";
     }
 
