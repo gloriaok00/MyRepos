b5e893bec4d540f75607c694ed7b5dcdd94b3e01
zhangyu <zhangyu121.ok@163.com>
2022-03-01 22:44:10 +0800

    枚举基础的三个方法

--- demo/src/main/java/com/example/demo/pingshi/enumpractise/EnumDemo.java
+++ demo/src/main/java/com/example/demo/pingshi/enumpractise/EnumDemo.java
@@ -2,19 +2,14 @@ package com.example.demo.pingshi.enumpractise;
 
 /**
  * @author zhangyu
- * @description 翻译优化
+ * @description 枚举基础
  * @date 2022-02-25 11:02
  */
 
 public class EnumDemo {
     public static void main(String[] args) {
-        for (EnumWeek value : EnumWeek.values()) {
-            System.out.println(value.getName());
-          /*  String gt = HttpUtil.get("http://fanyi.youdao.com/translate?&doctype=json&type=AUTO&i="+ value.getName(),null,1000);
-            JSONObject obj = JSONObject.parseObject(gt);
-            JSONObject sss = obj.getJSONArray("translateResult").getJSONArray(0).getObject(0, JSONObject.class);
-            System.out.println(sss.getString("tgt"));
-            String tgt = sss.getString("tgt");*/
+        for (EnumWeek week:EnumWeek.values()){
+            System.out.println("我的索引:"+week.ordinal()+" 我的CODE:"+week.getCode() + " 我的名称:"+week.getName());
         }
         System.out.println(EnumWeek.valueOf("Monday"));
     }
