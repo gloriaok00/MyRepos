be4ef00e9dea0a345e92ff6bc8ad463c3a8c3ae0
zhangyu <zhangyu121.ok@163.com>
2022-03-12 20:29:50 +0800

    java修饰符再学习

--- demo/src/main/java/com/example/demo/pingshi/javamodifier/ModifierDemo.java
+++ demo/src/main/java/com/example/demo/pingshi/javamodifier/ModifierDemo.java
@@ -1,10 +1,11 @@
-package com.example.demo.pingshi.javamodifier.sub;
+package com.example.demo.pingshi.javamodifier;
 
-import com.example.demo.pingshi.javamodifier.MyModifier;
+import com.example.demo.pingshi.javamodifier.sub.MyModifier;
 import org.junit.Test;
 
 /**
- * @description
+ * @description https://blog.csdn.net/menglanyingfei/article/details/55210673
+ * 结合这个帖子，把MyModifier从sub里拿出来到与ModifierDemo同层看看两个方法的编译效果，再拿回来也看看
  * @date 2021-12-02 14:13
  * @date 2022-03-12 13:53
  */
@@ -14,13 +15,13 @@ public class ModifierDemo extends MyModifier {
     @Test
     public void show() {
         MyModifier myModifier = new MyModifier();
-        System.out.println(myModifier.b);
-        System.out.println(myModifier.c);
+       /* System.out.println(myModifier.b);
+        System.out.println(myModifier.c);*/
     }
 
     @Test
     public void dd(){
-        System.out.println(b);
-        System.out.println(c);
+       /* System.out.println(b);
+        System.out.println(c);*/
     }
 }
--- demo/src/main/java/com/example/demo/pingshi/javamodifier/sub/MyModifier.java
+++ demo/src/main/java/com/example/demo/pingshi/javamodifier/sub/MyModifier.java
@@ -1,15 +1,16 @@
-package com.example.demo.pingshi.javamodifier;
+package com.example.demo.pingshi.javamodifier.sub;
 
 /**
  * @description
  * @date 2021-11-09 15:31
+ * @date 2022-03-12 13:53
  */
 
 public class MyModifier {
 
     private int a = 2;
-    protected String b = "我是protected的字段";
-    String c = "friendly与project的区别老是记不住";
+    String b = "friendly与project的区别老是记不住";
+    protected String c = "我是protected的字段";
     public String d = "长点脑子，只要用了private修饰的变量，子类也用不了";
 
 }
