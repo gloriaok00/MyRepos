aa3f4f6821b55a38c93ed1681eabd5f443d36f27
zhangyu <zhangyu121.ok@163.com>
2022-03-27 23:13:15 +0800

    bug修复 放到ext下，在里面再建文件夹里是不行的

--- demo/src/main/java/com/example/demo/pingshi/classloader/HMain.java
+++ demo/src/main/java/com/example/demo/pingshi/classloader/HMain.java
@@ -4,8 +4,9 @@ import groovy.sql.GroovyResultSetExtension;
 
 /**
  * @author zhangyu
- * @description 把groovy-all-1.1-rc-1.jar也放到/Library/Java/JavaVirtualMachines/jdk1.8.0_171.jdk/Contents/Home/jre/lib/ext/grxx
- * 里一份，类加载器就只剩ExtClassLoader了
+ * @description 把groovy-all-1.1-rc-1.jar也放到/Library/Java/JavaVirtualMachines/jdk1.8.0_171.jdk/Contents/Home/jre/lib/ext/
+ * 里一份，类加载器会为ExtClassLoader
+ * 把放在ext下的拿走，就是AppClassLoader了
  * @date 2021-10-23 13:37 生日快乐 宝贝
  */
 
@@ -13,9 +14,8 @@ public class HMain {
 
     public static void main(String[] args) {
         ClassLoader xx = GroovyResultSetExtension.class.getClassLoader();
-        while (xx != null) {
+        if (xx != null) {
             System.out.println(xx);
-            xx = xx.getParent();
         }
     }
 }
