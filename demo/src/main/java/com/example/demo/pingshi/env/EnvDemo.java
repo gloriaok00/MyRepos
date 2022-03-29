3c5f61c8d5508887f53d27c9d0c9e3725b3a937a
zhangyu <zhangyu121.ok@163.com>
2022-03-29 22:17:09 +0800

    System.getEnv学习

--- demo/src/main/java/com/example/demo/pingshi/env/EnvDemo.java
+++ demo/src/main/java/com/example/demo/pingshi/env/EnvDemo.java
@@ -1,14 +1,20 @@
 package com.example.demo.pingshi.env;
 
+import java.util.Map;
+
 /**
- * @author zhangyu
- * @date 2019-10-27 18:20
+ * @date 2022-03-29 21:48
  */
 public class EnvDemo {
 
     public static void main(String[] args) {
-        //String env=System.getenv("JAVA_HOME");
-        String env = System.getenv("TESTNEW");
+        String env=System.getenv("JAVA_HOME");
+        String env1=System.getProperty("java.home");
         System.out.println(env);
+        System.out.println(env1);
+        Map<String,String> envMap=System.getenv();
+        envMap.forEach((k,v)->{
+            System.out.println(k+":"+v);
+        });
     }
 }
