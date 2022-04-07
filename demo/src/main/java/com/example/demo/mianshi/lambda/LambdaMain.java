18496b759ee2c076990acb20cf18c9441cf4be47
zhangyu <zhangyu121.ok@163.com>
2022-04-07 09:18:05 +0800

    default学习

--- demo/src/main/java/com/example/demo/mianshi/lambda/LambdaMain.java
+++ demo/src/main/java/com/example/demo/mianshi/lambda/LambdaMain.java
@@ -3,6 +3,7 @@ package com.example.demo.mianshi.lambda;
 /**
  * @author zhangyu
  * @date 2019-08-21 16:28
+ * @date 2022-04-06 20:25
  */
 public class LambdaMain {
     public void show(MyInterf myInterf) {
@@ -13,8 +14,8 @@ public class LambdaMain {
 
     public static void main(String[] args) {
         LambdaMain demo = new LambdaMain();
-        demo.show((i) -> System.out.println("函数式接口中的test" + i));
-        MyInterf ins = (j) -> System.out.println("lambda是对象");
+        demo.show((i) -> System.out.println("现在学函数式接口，才看懂" + i));
+        MyInterf ins = (j) -> System.out.println("这样也行");
         System.out.println(ins.getClass());
     }
 }
