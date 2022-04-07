3fd82518090c7244a13a4706d22c785b0706ff76
zhangyu <zhangyu121.ok@163.com>
2022-04-07 10:50:44 +0800

    细节调整

--- demo/src/main/java/com/example/demo/mianshi/lambda/practise/MFakeConsumer.java
+++ demo/src/main/java/com/example/demo/mianshi/lambda/practise/MFakeConsumer.java
@@ -1,8 +1,6 @@
 package com.example.demo.mianshi.lambda.practise;
 
 import java.util.HashSet;
-import java.util.Set;
-import java.util.function.Predicate;
 
 /**
  * @description 综合练习 模仿consumer功能
@@ -10,17 +8,7 @@ import java.util.function.Predicate;
  */
 
 public class MFakeConsumer {
-
-    public static void ppp(Set<String> data, Predicate<String> predicate){
-        data.forEach(e->{
-            if(predicate.test("b")){
-                data.remove(e);
-            }
-        });
-        System.out.println("====after");
-        data.forEach(System.out::println);
-    }
-
+    
     public static void main(String[] args) {
         HashSet<String> datas = new HashSet<>();
         datas.add("a");
