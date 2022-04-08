d1a96a7962226411b6806e65bc79dd67f39c1655
zhangyu <zhangyu121.ok@163.com>
2022-04-08 10:41:27 +0800

    不再纠结这里参数省略的形式啥的了 多写写 自然就好了

--- demo/src/main/java/com/example/demo/mianshi/lambda/p1/MOneParamFi.java
+++ demo/src/main/java/com/example/demo/mianshi/lambda/p1/MOneParamFi.java
@@ -9,6 +9,7 @@ public class MOneParamFi {
 
     public static void main(String[] args) {
         //lambda可选的参数圆括号：一个参数无需定义圆括号，但多个参数需要定义圆括号
+        //() -> System.out.println("Hello world !")
         OneParamFI<String> oneParamFI = System.out::println;
         oneParamFI.showAnything("对抗寂寞");
     }
