ddb64b5bc2dba5c5ac154307695c9ba2a5d60568
zhangyu <zhangyu121.ok@163.com>
2022-04-13 18:15:42 +0800

    mm第三版

--- xx/src/main/java/org/example/mmm/MM.java
+++ xx/src/main/java/org/example/mmm/MM.java
@@ -3,9 +3,16 @@ package org.example.mmm;
 import cn.hutool.http.HttpUtil;
 import io.minio.*;
 import io.minio.errors.*;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.InvalidKeyException;
@@ -16,13 +23,21 @@ import java.security.NoSuchAlgorithmException;
  * @date 2022-04-12 16:13
  */
 
+@Slf4j
+@Component
 public class MM {
 
-   static MinioClient minioClient =
-            MinioClient.builder()
-                    .endpoint("http://223.223.176.32:30711")
-                    .credentials("OKw3bxT5me", "AnI22zu!vu89FaJ")
-                    .build();
+    @Value("${minio.endpoint}")
+    private String endpoint;
+
+    @Value("${minio.accessKey}")
+    private String accessKey;
+
+    @Value("${minio.secretKey}")
+    private String secretKey;
+
+
+    MinioClient minioClient=null;
 
     public void upload() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
         minioClient.uploadObject(
@@ -46,8 +61,13 @@ public class MM {
         System.out.println("done");
     }
 
-    public static void main(String[] args) throws Exception {
-        MM mm=new MM();
+    @PostConstruct
+    public void initDefaultData() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        minioClient =
+                MinioClient.builder()
+                        .endpoint(endpoint)
+                        .credentials(accessKey, secretKey)
+                        .build();
         boolean flag = minioClient.bucketExists(BucketExistsArgs.builder().bucket("iot2").build());
         if(!flag){
             minioClient.makeBucket(MakeBucketArgs.builder()
@@ -55,9 +75,9 @@ public class MM {
                     .build());
             String ss="{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"AWS\":[\"*\"]},\"Action\":[\"s3:GetBucketLocation\",\"s3:ListBucket\",\"s3:ListBucketMultipartUploads\"],\"Resource\":[\"arn:aws:s3:::iot2\"]},{\"Effect\":\"Allow\",\"Principal\":{\"AWS\":[\"*\"]},\"Action\":[\"s3:DeleteObject\",\"s3:GetObject\",\"s3:ListMultipartUploadParts\",\"s3:PutObject\",\"s3:AbortMultipartUpload\"],\"Resource\":[\"arn:aws:s3:::iot2/*\"]}]}";
             minioClient.setBucketPolicy(SetBucketPolicyArgs.builder().bucket("iot2").config(ss).build());
-            mm.upload();
+            this.upload();
         }else{
-            mm.uploadOthers();
+            this.uploadOthers();
         }
     }
 
