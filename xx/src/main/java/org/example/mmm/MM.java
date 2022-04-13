35946aae106c6382b9428419a36195548a8079c1
zhangyu <zhangyu121.ok@163.com>
2022-04-13 17:59:45 +0800

    第二版

--- xx/src/main/java/org/example/mmm/MM.java
+++ xx/src/main/java/org/example/mmm/MM.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.rmi.ServerException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
@@ -25,31 +24,30 @@ public class MM {
                     .credentials("OKw3bxT5me", "AnI22zu!vu89FaJ")
                     .build();
 
+    public void upload() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        minioClient.uploadObject(
+                UploadObjectArgs.builder()
+                        .bucket("iot2")
+                        .object("default/areaType/default_areaType.jpg")
+                        .filename("/Users/zhangyu/Downloads/default_areaType.jpg")
+                        .build());
+        System.out.println("done");
+    }
 
-    /* @Test
-    public void upload() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException{
-        // Make 'asiatrip' bucket if not exist.
-
-        boolean found =
-                minioClient.bucketExists(BucketExistsArgs.builder().bucket("iot").build());
-        if (!found) {
-            // Make a new bucket called 'asiatrip'.
-            minioClient.makeBucket(MakeBucketArgs.builder().bucket("iot").build());
-        } else {
-            System.out.println("Bucket 'iot' already exists.");
-        }
-
+    public void uploadOthers() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
         // Upload '/home/user/Photos/asiaphotos.zip' as object name 'asiaphotos-2015.zip' to bucket
         // 'asiatrip'.
         minioClient.uploadObject(
                 UploadObjectArgs.builder()
-                        .bucket("iot")
-                        .object("zytest.txt")
-                        .filename("/Users/zhangyu/zytest.txt")
+                        .bucket("iot2")
+                        .object("default/areaType/桌面.jpg")
+                        .filename("/Users/zhangyu/Downloads/桌面.jpg")
                         .build());
         System.out.println("done");
-    }*/
+    }
+
     public static void main(String[] args) throws Exception {
+        MM mm=new MM();
         boolean flag = minioClient.bucketExists(BucketExistsArgs.builder().bucket("iot2").build());
         if(!flag){
             minioClient.makeBucket(MakeBucketArgs.builder()
@@ -57,6 +55,9 @@ public class MM {
                     .build());
             String ss="{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"AWS\":[\"*\"]},\"Action\":[\"s3:GetBucketLocation\",\"s3:ListBucket\",\"s3:ListBucketMultipartUploads\"],\"Resource\":[\"arn:aws:s3:::iot2\"]},{\"Effect\":\"Allow\",\"Principal\":{\"AWS\":[\"*\"]},\"Action\":[\"s3:DeleteObject\",\"s3:GetObject\",\"s3:ListMultipartUploadParts\",\"s3:PutObject\",\"s3:AbortMultipartUpload\"],\"Resource\":[\"arn:aws:s3:::iot2/*\"]}]}";
             minioClient.setBucketPolicy(SetBucketPolicyArgs.builder().bucket("iot2").config(ss).build());
+            mm.upload();
+        }else{
+            mm.uploadOthers();
         }
     }
 
