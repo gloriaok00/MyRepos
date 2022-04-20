4d365ab444ee03daf17cd88b0ece9a345596174b
zhangyu <zhangyu121.ok@163.com>
2022-04-20 18:25:51 +0800

    list remove exception学习

---
+++ demo/src/main/java/com/example/demo/pingshi/datatype/ListRemove.java
@@ -0,0 +1,29 @@
+package com.example.demo.pingshi.datatype;
+
+import com.google.common.collect.Lists;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @description list remove学习
+ * @date 2022-04-20 18:03
+ */
+
+public class ListRemove {
+
+    public static void main(String[] args) {
+        String pattern = " ^, v, m, n-music-name, $ ";
+        String[] patternSplit = pattern.split(",");
+
+        // 去除模式中的^和$标识
+        //想起来了 Arrays.asList 里调的是AbstractList里的remove方法会报UnsupportedOperationException
+        List<String> natureList = Arrays.asList(patternSplit);
+        List<String> newCopylist=Lists.newCopyOnWriteArrayList(natureList);
+        //natureList.forEach(System.out::println);
+        newCopylist.remove(0);
+        //System.out.println();
+        newCopylist.forEach(System.out::println);
+
+    }
+}
