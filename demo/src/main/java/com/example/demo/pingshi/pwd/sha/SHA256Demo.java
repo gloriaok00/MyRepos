f4f81f13cd4dfd5863f42bf15fa577c28addc263
zhangyu <zhangyu121.ok@163.com>
2022-04-22 22:49:29 +0800

    SHA256练习

--- demo/src/main/java/com/example/demo/pingshi/pwd/sha/SHA256Demo.java
+++ demo/src/main/java/com/example/demo/pingshi/pwd/sha/SHA256Demo.java
@@ -1,5 +1,8 @@
 package com.example.demo.pingshi.pwd.sha;
 
+import org.junit.Test;
+
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
@@ -22,12 +25,21 @@ public class SHA256Demo {
         StringBuilder sb = new StringBuilder();
         //对哈希数进行密码散列计算
         for (byte item : array) {
-            sb.append(Integer.toHexString((item & 0xFF)|0x100).substring(1, 3));
+            sb.append(Integer.toHexString((item & 0xFF) | 0x100).substring(1, 3));
         }
         return sb.toString().toUpperCase();
     }
 
     public static void main(String[] args) throws Exception {
         System.out.println(getSHA256("admin"));
+    }
+
+    @Test
+    public void sha256Another() throws Exception {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update("Hello".getBytes(StandardCharsets.UTF_8));
+        md.update("World".getBytes(StandardCharsets.UTF_8));
+        byte[] result = md.digest();
+        System.out.println(new BigInteger(1, result).toString(16));
     }
 }
