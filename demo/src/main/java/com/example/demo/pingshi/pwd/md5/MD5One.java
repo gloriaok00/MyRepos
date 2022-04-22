9f1622f357ba2415db3365bb74d62aa9d9db69c6
zhangyu <zhangyu121.ok@163.com>
2022-04-22 22:42:22 +0800

    哈希算法简单实现学习

--- demo/src/main/java/com/example/demo/pingshi/pwd/md5/MD5One.java
+++ demo/src/main/java/com/example/demo/pingshi/pwd/md5/MD5One.java
@@ -1,4 +1,7 @@
-package com.example.demo.pingshi.pwd;
+package com.example.demo.pingshi.pwd.md5;
+
+import cn.hutool.crypto.digest.DigestUtil;
+import org.junit.Test;
 
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
@@ -6,11 +9,11 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * @description MD5练习 哈希算法中的一种实现
+ * @description MD5练习 哈希算法中的一种实现 没有盐
  * @date 2022-04-22 16:58
  */
 
-public class MD5Demo {
+public class MD5One {
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
         //创建一个MessageDigest实例
@@ -22,4 +25,12 @@ public class MD5Demo {
         //转换为十六进制的字符串
         System.out.println(new BigInteger(1, result).toString(16));
     }
+
+    @Test
+    public void MD5Test() {
+        String password = "abc123456";
+        String md5 = DigestUtil.md5Hex(password);
+        System.out.println("md5 = " + md5);
+    }
+
 }
