9251032081a998516685a663e2332f848db35e47
zhangyu <zhangyu121.ok@163.com>
2022-04-22 11:00:24 +0800

    java regex学习

--- demo/src/main/java/com/example/demo/pingshi/myregex/MyRegex.java
+++ demo/src/main/java/com/example/demo/pingshi/myregex/MyRegex.java
@@ -1,5 +1,7 @@
 package com.example.demo.pingshi.myregex;
 
+import java.util.regex.Pattern;
+
 /**
  * @description
  * @date 2021-11-11 15:27
@@ -7,9 +9,14 @@ package com.example.demo.pingshi.myregex;
 
 public class MyRegex {
     public static void main(String[] args) {
+        //方式1
         String regex = ".+\\d+.+";
         String str = "sdasd12dsf";
         System.out.println(str.matches(regex));
-        //Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        //方式2
+        //mongodb xx.put("nn", Pattern.compile("^.*" + cc +".*$",Pattern.CASE_INSENSITIVE));
+        Pattern p=Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        //方式3
+        System.out.println(Pattern.matches(regex,str));
     }
 }
