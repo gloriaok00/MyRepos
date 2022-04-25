5b7dd8c791bd19e1abfe03255d77c72f47315c44
zhangyu <zhangyu121.ok@163.com>
2022-04-25 23:48:41 +0800

    泛型定义在方法中

---
+++ demo/src/main/java/com/example/demo/pingshi/datatype/fanxing/fx4/FX4.java
@@ -0,0 +1,14 @@
+package com.example.demo.pingshi.datatype.fanxing.fx4;
+
+/**
+ * @description 泛型定义在方法中
+ * @date 2022-04-25 23:46
+ */
+
+public class FX4 {
+
+    //定义在方法中的泛型
+    public <TTT> void show(TTT ttt){
+        System.out.println(ttt);
+    }
+}
