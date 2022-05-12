fb7d5648c6471b559c03b5fc6d204bd94c700823
zhangyu <zhangyu121.ok@163.com>
2022-05-12 16:11:48 +0800

    随机练习

--- demo/src/main/java/com/example/demo/pingshi/initorder/MyOrder.java
+++ demo/src/main/java/com/example/demo/pingshi/initorder/MyOrder.java
@@ -1,12 +1,9 @@
 package com.example.demo.pingshi.initorder;
 
 /**
- * @author zhangyu
- * @description [静态变量/静态方法/静态块] [普通变量/普通方法/非静态块] [构造函数]
- * 1.所有方法无论静态还是非静态，只是实例化。不是执行
- * 2.父类>子类 静态>非静态
- * 3.父类普通然后执行父类构造函数再子类普通再子类构造器。而不是父类普通然后子类普通
+ * @description 随机练习
  * @date 2021-10-28 09:10
+ * @date 2022-05-12 16:11
  */
 
 public class MyOrder {
