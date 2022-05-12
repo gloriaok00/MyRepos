62f8926fb9ddde9c7d4b2b86bef48e280e57b1d0
zhangyu <zhangyu121.ok@163.com>
2022-05-12 13:35:13 +0800

    类以及对象初始化顺序

--- demo/src/main/java/com/example/demo/pingshi/initorder/i1/I1.java
+++ demo/src/main/java/com/example/demo/pingshi/initorder/i1/I1.java
@@ -1,6 +1,6 @@
-package com.example.demo.pingshi.initorder;
+package com.example.demo.pingshi.initorder.i1;
 
-public class InitialOrderWithoutExtend {
+public class I1 {
     static SampleClass sam = new SampleClass("静态成员sam初始化");
     SampleClass sam1 = new SampleClass("普通成员sam1初始化");
 
@@ -14,18 +14,18 @@ public class InitialOrderWithoutExtend {
 
     SampleClass sam2 = new SampleClass("普通成员sam2初始化");
 
-    InitialOrderWithoutExtend() {
+    I1() {
         System.out.println("InitialOrderWithoutExtend默认构造函数被调用");
     }
 
     public static void main(String[] args) {
         // 创建第1个主类对象
         System.out.println("第1个主类对象：");
-        InitialOrderWithoutExtend ts = new InitialOrderWithoutExtend();
+        I1 ts = new I1();
 
         // 创建第2个主类对象
         System.out.println("第2个主类对象：");
-        InitialOrderWithoutExtend ts2 = new InitialOrderWithoutExtend();
+        I1 ts2 = new I1();
 
         // 查看两个主类对象的静态成员：
         System.out.println("2个主类对象的静态对象：");
@@ -35,11 +35,6 @@ public class InitialOrderWithoutExtend {
 }
 
 class SampleClass {
-    // SampleClass 不能包含任何主类InitialOrderWithoutExtend的成员变量
-    // 否则导致循环引用，循环初始化，调用栈深度过大
-    // 抛出 StackOverFlow 异常
-    // static InitialOrderWithoutExtend iniClass1 = new InitialOrderWithoutExtend("静态成员iniClass1初始化");
-    // InitialOrderWithoutExtend iniClass2 = new InitialOrderWithoutExtend("普通成员成员iniClass2初始化");
 
     String s;
 
