9cb3ce738a4b602a44ccb7f782fd6a194c688d05
zhangyu <zhangyu121.ok@163.com>
2022-05-29 23:58:22 +0800

    补充提交mybatis

---
+++ demo/src/main/java/com/example/demo/mbs/cus/dao/UUserMapper.java
@@ -0,0 +1,16 @@
+package com.example.demo.mbs.cus.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.demo.mbs.cus.dto.UUser;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @author ${author}
+ * @since 2022-05-17
+ */
+public interface UUserMapper extends BaseMapper<UUser> {
+
+}
