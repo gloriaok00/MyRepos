4734cc48d529c1bd6d39016cb7a86631cd213436
zhangyu <zhangyu121.ok@163.com>
2022-07-22 13:56:52 +0800

    servlet相关对象学习

--- cc/src/main/java/MyFilter.java
+++ cc/src/main/java/MyFilter.java
@@ -11,6 +11,12 @@ import java.io.IOException;
 public class MyFilter implements Filter {
 
     @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("全局:"+filterConfig.getServletContext().getInitParameter("globalParam"));
+        System.out.println("自身:"+filterConfig.getInitParameter("filterInitParam"));
+    }
+
+    @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response=(HttpServletResponse)servletResponse;
         response.setContentType("text/html; charset=utf-8");
