605b1b04ec47ae82faa5e52b99be3f1dc60fc70c
zhangyu <zhangyu121.ok@163.com>
2022-07-27 22:17:17 +0800

    springboot部署成war到tomcat

---
+++ xx/src/main/java/org/example/AppApplication.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class AppApplication extends SpringBootServletInitializer
+{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(AppApplication.class);
+    }
+
+}
