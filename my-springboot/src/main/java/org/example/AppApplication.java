d63f5f050ba8638c77b0b2c61dc15489c662e78c
zhangyu <zhangyu121.ok@163.com>
2022-08-14 23:10:50 +0800

    Revert "修改基础骨架项目 去掉mysql依赖"
    This reverts commit 7b1196902941242dc1d6a7f2e599fb509c836e22.

--- my-springboot/src/main/java/org/example/AppApplication.java
+++ my-springboot/src/main/java/org/example/AppApplication.java
@@ -1,15 +1,14 @@
 package org.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-public class AppApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(AppApplication.class, args);
+public class AppApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(AppApplication.class,args);
     }
 }
