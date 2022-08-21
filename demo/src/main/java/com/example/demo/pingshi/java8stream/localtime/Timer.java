322937b01f0f241121486d181ebeb58ed588019c
zhangyu <zhangyu121.ok@163.com>
2022-08-21 14:56:20 +0800

    git reset没整明白

--- demo/src/main/java/com/example/demo/pingshi/java8stream/localtime/Timer.java
+++ demo/src/main/java/com/example/demo/pingshi/java8stream/localtime/Timer.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * @description java8 time新写法 git revert--从staged变回modified
+ * @description java8 time新写法
  * @date 2022/8/21 01:55
  */
 
