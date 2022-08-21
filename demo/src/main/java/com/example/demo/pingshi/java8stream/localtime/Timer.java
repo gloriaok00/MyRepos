287e0c3dc4e593b50816b392953d603fa5155ea1
zhangyu <zhangyu121.ok@163.com>
2022-08-21 14:05:30 +0800

    timer update

--- demo/src/main/java/com/example/demo/pingshi/java8stream/localtime/Timer.java
+++ demo/src/main/java/com/example/demo/pingshi/java8stream/localtime/Timer.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * @description java8 time新写法
+ * @description java8 time新写法 git revert--从staged变回modified
  * @date 2022/8/21 01:55
  */
 
