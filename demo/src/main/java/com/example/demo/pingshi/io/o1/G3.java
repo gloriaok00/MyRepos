f02d075e502e106c6f7ab64af4779bb86b050e97
zhangyu <zhangyu121.ok@163.com>
2022-09-20 02:50:21 +0800

    io随机练习

---
+++ demo/src/main/java/com/example/demo/pingshi/io/o1/G3.java
@@ -0,0 +1,24 @@
+package com.example.demo.pingshi.io.o1;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @description io随机练习
+ * @date 2022/9/20 02:43
+ */
+
+public class G3 {
+
+    public static void main(String[] args) throws IOException {
+        InputStream is = new FileInputStream("/Users/zhangyu/Desktop/test.txt");
+        String str = "for tonight i am yours.. so deny the truth";
+        byte[] bs = str.getBytes(StandardCharsets.UTF_8);
+        OutputStream os = new FileOutputStream("/Users/zhangyu/file-1.txt");
+        for (byte b : bs) {
+            os.write(b);
+        }
+        os.close();
+        is.close();
+    }
+}
