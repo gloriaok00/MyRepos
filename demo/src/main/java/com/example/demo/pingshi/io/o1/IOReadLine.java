e0c67ed8c77b4d7802cf9232d7b92b43b7721336
zhangyu <zhangyu121.ok@163.com>
2022-09-21 09:52:10 +0800

    io read line

---
+++ demo/src/main/java/com/example/demo/pingshi/io/o1/IOReadLine.java
@@ -0,0 +1,30 @@
+package com.example.demo.pingshi.io.o1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * @description InputStreamReader line用法
+ * @date 2022/9/21 01:32
+ */
+
+public class IOReadLine {
+
+    public static void main(String[] args) {
+
+        try {
+            InputStream stream = IOReadLine.class.getClassLoader().getResourceAsStream("bb.txt");
+            InputStreamReader in = new InputStreamReader(stream);
+            BufferedReader reader = new BufferedReader(in);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
