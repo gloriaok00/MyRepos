18f2fa93037abf6ddc44293047469079e0799733
zhangyu <zhangyu121.ok@163.com>
2022-09-26 20:07:05 +0800

    join

---
+++ demo/src/main/java/com/example/demo/mianshi/thread/t2/Join02.java
@@ -0,0 +1,49 @@
+package com.example.demo.mianshi.thread.t2;
+
+public class Join02 {
+
+    public static void main(String[] args) {
+
+        ThreadBoy boy = new ThreadBoy();
+        boy.start();
+
+    }
+    
+    static class ThreadBoy extends Thread{
+        @Override
+        public void run() {
+
+            System.out.println("男孩和女孩准备出去逛街");
+
+            ThreadGirl girl = new ThreadGirl();
+            girl.start();
+
+            try {
+                girl.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("男孩和女孩开始去逛街了");
+        }
+    }
+
+    static class ThreadGirl extends Thread{
+        @Override
+        public void run() {
+            int time = 5000;
+
+            System.out.println("女孩开始化妆,男孩在等待。。。");
+
+            try {
+                Thread.sleep(time);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("女孩化妆完成！，耗时" + time);
+
+        }
+    }
+
+}
