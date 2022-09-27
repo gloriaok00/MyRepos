7c3feb842a2f96f12ec250b1cf504ee44fa63a31
zhangyu <zhangyu121.ok@163.com>
2022-09-27 12:04:26 +0800

    Interrupt练习

---
+++ demo/src/main/java/com/example/demo/mianshi/thread/t5/Main.java
@@ -0,0 +1,46 @@
+package com.example.demo.mianshi.thread.t5;
+
+/**
+ * @description interrupt随机练习
+ * @date 2022/9/27 12:03
+ */
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new MyThread();
+        t.start();
+        Thread.sleep(1000);
+        t.interrupt(); // 中断t线程
+        t.join(); // 等待t线程结束
+        System.out.println("end");
+    }
+}
+
+class MyThread extends Thread {
+    public void run() {
+        Thread hello = new HelloThread();
+        hello.start(); // 启动hello线程
+        try {
+            hello.join(); // 等待hello线程结束
+        } catch (InterruptedException e) {
+            System.out.println("interrupted!");
+        }
+        System.out.println("抛出InterruptedException后继续执行");
+        //hello.interrupt();
+    }
+}
+
+class HelloThread extends Thread {
+    public void run() {
+        int n = 0;
+        while (!isInterrupted()) {
+            n++;
+            System.out.println(n + " hello!");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println("hello InterruptedException");
+                break;
+            }
+        }
+    }
+}
