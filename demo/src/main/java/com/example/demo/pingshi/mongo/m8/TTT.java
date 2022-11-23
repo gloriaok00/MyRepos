199f7790f853442e2e7bf9c0b7b10a637b34e5b4
zhangyu <zhangyu121.ok@163.com>
2022-11-23 09:58:48 +0800

    工具类更新

--- demo/src/main/java/com/example/demo/pingshi/mongo/m8/TTT.java
+++ demo/src/main/java/com/example/demo/pingshi/mongo/m8/TTT.java
@@ -8,6 +8,8 @@ import org.bson.UuidRepresentation;
 import org.bson.internal.UuidHelper;
 import org.bson.types.Binary;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -22,27 +24,31 @@ public class TTT {
 
         MongoClient client = new MongoClient("localhost:27017");
         MongoDatabase iotdb = client.getDatabase("Foo");
-        Binary bb = new Binary((byte) 3, UuidHelper.encodeUuidToBinary(UUID.fromString("a2999e3f-5cc9-4b15-b5cc-20b237c875e9"), UuidRepresentation.JAVA_LEGACY));
+        List<Document> list = iotdb.getCollection("task").find().into(new ArrayList<>());
+        list.forEach(e -> {
+            //System.out.println(e.get("_id").toString());
+            Binary bb = new Binary((byte) 3, UuidHelper.encodeUuidToBinary(UUID.fromString(e.get("_id").toString()), UuidRepresentation.JAVA_LEGACY));
 
-        Document query = new Document();
-        query.put("_id", bb);
+            Document query = new Document();
+            query.put("_id", bb);
 
 
-        Document one = iotdb.getCollection("task").find(query).first();
-        Double bt=Double.parseDouble(one.getString("batchType"));
+            Document one = iotdb.getCollection("task").find(query).first();
+            Double bt = Double.parseDouble(one.getString("batchType"));
 
-        System.out.println("getting one:" + bt);
+            System.out.println("getting one:" + bt);
 
-        Document mm = new Document();
-        mm.put("batchType", 1);
-        Document doc = new Document();
-        doc.put("$unset", mm);
-        iotdb.getCollection("task").updateOne(query, doc);
-        Document mmNew = new Document();
-        mmNew.put("batchType", bt);
-        Document docNew = new Document();
-        docNew.put("$set", mmNew);
-        iotdb.getCollection("task").updateOne(query, docNew);
+            Document mm = new Document();
+            mm.put("batchType", 1);
+            Document doc = new Document();
+            doc.put("$unset", mm);
+            iotdb.getCollection("task").updateOne(query, doc);
+            Document mmNew = new Document();
+            mmNew.put("batchType", bt);
+            Document docNew = new Document();
+            docNew.put("$set", mmNew);
+            iotdb.getCollection("task").updateOne(query, docNew);
+        });
 
     }
 }
