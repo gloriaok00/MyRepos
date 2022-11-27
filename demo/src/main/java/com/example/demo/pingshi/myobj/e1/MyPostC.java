c7c69b49871ab7100c2bd52547e1ac945ee8d514
zhangyu <zhangyu121.ok@163.com>
2022-11-28 02:30:06 +0800

    毋庸置疑 static块作为类级别的启动还是最优先的

---
+++ demo/src/main/java/com/example/demo/pingshi/myobj/e1/MyPostC.java
@@ -0,0 +1,23 @@
+package com.example.demo.pingshi.myobj.e1;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @description
+ * @date 2022/11/28 02:22
+ */
+
+@RestController
+public class MyPostC {
+
+    @PostConstruct
+    public void xx(){
+        System.out.println("2.我是PostConstruct");
+    }
+
+    static {
+        System.out.println("1.我是static啊啊啊 查看加载顺序");
+    }
+}
