ab0c626b43c5df413b3d619470fd21ca0b79eb23
zhangyu <zhangyu121.ok@163.com>
2022-12-21 23:48:35 +0800

    ArrayList add添加时的不安全情况模拟

---
+++ demo/src/main/java/com/example/demo/pingshi/mylist/l2/L2.java
@@ -0,0 +1,54 @@
+package com.example.demo.pingshi.mylist.l2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description ArrayList add添加时的不安全情况模拟
+ * @date 2022/12/21 23:43
+ */
+
+public class L2 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        final List<Integer> list = new ArrayList<Integer>();
+
+        // 线程A将0-10000添加到list
+        new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    list.add(i);
+
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+        // 线程B将10000-20000添加到列表
+        new Thread(new Runnable() {
+            public void run() {
+                for (int i = 10000; i < 20000; i++) {
+                    list.add(i);
+
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+        Thread.sleep(1000);
+
+        // 打印所有结果
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("第" + (i + 1) + "个元素为：" + list.get(i));
+        }
+    }
+}
