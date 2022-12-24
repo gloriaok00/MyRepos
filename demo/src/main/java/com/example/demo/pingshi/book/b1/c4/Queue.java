ba821f7c6d699bc20aaa2028e3828332ad2a350a
zhangyu <zhangyu121.ok@163.com>
2022-12-24 20:00:53 +0800

    queue练习

--- demo/src/main/java/com/example/demo/pingshi/book/b1/c4/Queue.java
+++ demo/src/main/java/com/example/demo/pingshi/book/b1/c4/Queue.java
@@ -1,92 +1,78 @@
 package com.example.demo.pingshi.book.b1.c4;
 
-// Queue.java
-// demonstrates queue
-// to run this program: C>java QueueApp
-////////////////////////////////////////////////////////////////
-class Queue
-   {
-   private int maxSize;
-   private long[] queArray;
-   private int front;
-   private int rear;
-   private int nItems;
-//--------------------------------------------------------------
-   public Queue(int s)          // constructor
-      {
-      maxSize = s;
-      queArray = new long[maxSize];
-      front = 0;
-      rear = -1;
-      nItems = 0;
-      }
-//--------------------------------------------------------------
-   public void insert(long j)   // put item at rear of queue
-      {
-      if(rear == maxSize-1)         // deal with wraparound
-         rear = -1;
-      queArray[++rear] = j;         // increment rear and insert
-      nItems++;                     // one more item
-      }
-//--------------------------------------------------------------
-   public long remove()         // take item from front of queue
-      {
-      long temp = queArray[front++]; // get value and incr front
-      if(front == maxSize)           // deal with wraparound
-         front = 0;
-      nItems--;                      // one less item
-      return temp;
-      }
-//--------------------------------------------------------------
-   public long peekFront()      // peek at front of queue
-      {
-      return queArray[front];
-      }
-//--------------------------------------------------------------
-   public boolean isEmpty()    // true if queue is empty
-      {
-      return (nItems==0);
-      }
-//--------------------------------------------------------------
-   public boolean isFull()     // true if queue is full
-      {
-      return (nItems==maxSize);
-      }
-//--------------------------------------------------------------
-   public int size()           // number of items in queue
-      {
-      return nItems;
-      }
-//--------------------------------------------------------------
-   }  // end class Queue
-////////////////////////////////////////////////////////////////
-class QueueApp
-   {
-   public static void main(String[] args)
-      {
-      Queue theQueue = new Queue(5);  // queue holds 5 items
+/**
+ * @description
+ * @date 2022/12/24 13:00
+ */
 
-      theQueue.insert(10);            // insert 4 items
-      theQueue.insert(20);
-      theQueue.insert(30);
-      theQueue.insert(40);
+public class Queue {
 
-      theQueue.remove();              // remove 3 items
-      theQueue.remove();              //    (10, 20, 30)
-      theQueue.remove();
+    public static void main(String[] args) {
+        QueueX qq = new QueueX(5);
+        qq.insert(1L);
+        qq.insert(2L);
 
-      theQueue.insert(50);            // insert 4 more items
-      theQueue.insert(60);            //    (wraps around)
-      theQueue.insert(70);
-      theQueue.insert(80);
+        qq.remove();
+        qq.insert(3L);
+        qq.insert(4L);
+        qq.insert(5L);
+        qq.insert(6L);
+        qq.insert(7L);
 
-      while( !theQueue.isEmpty() )    // remove and display
-         {                            //    all items
-         long n = theQueue.remove();  // (40, 50, 60, 70, 80)
-         System.out.print(n);
-         System.out.print(" ");
-         }
-      System.out.println("");
-      }  // end main()
-   }  // end class QueueApp
-////////////////////////////////////////////////////////////////
+        qq.remove();
+        qq.display();
+    }
+
+}
+
+class QueueX {
+
+    public Long[] queArray;
+    public int rear;
+    public int front;
+    public int nElems;
+    public int maxSize;
+
+    public QueueX(int size) {
+        front = 0;
+        rear = -1;
+        nElems = 0;
+        maxSize = size;
+        queArray = new Long[size];
+    }
+
+    public void insert(long e) {
+        if (nElems == maxSize) {
+            System.out.println("队列已满...无法再添加元素了");
+            return;
+        }
+        if (rear == maxSize - 1) {
+            rear = -1;
+        }
+        queArray[++rear] = e;
+        nElems++;
+    }
+
+    public long peekFront() {
+        return queArray[front];
+    }
+
+    public void remove() {
+        if (nElems == 0) {
+            System.out.println("队列已空...无法再移除元素了");
+            return;
+        }
+        if (front == maxSize) {
+            front = 0;
+        }
+        queArray[front++] = null;
+        nElems--;
+    }
+
+    public void display() {
+        for (int i = 0; i < 5; i++) {
+            System.out.print(queArray[i] + " ");
+        }
+    }
+
+}
