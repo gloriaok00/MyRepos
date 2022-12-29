eb3339f75ab3a511fb9b4eb4a1c16514f72268a7
zhangyu <zhangyu121.ok@163.com>
2022-12-29 20:38:04 +0800

    链表练习

--- demo/src/main/java/com/example/demo/pingshi/book/b1/c5/LinkedMain.java
+++ demo/src/main/java/com/example/demo/pingshi/book/b1/c5/LinkedMain.java
@@ -1,18 +1,23 @@
 package com.example.demo.pingshi.book.b1.c5;
 
 /**
- * @description
+ * @description 链表练习
  * @date 2022/12/26 23:44
+ * @date 2022/12/29 20:37
  */
 
 public class LinkedMain {
 
     public static void main(String[] args) {
+
         LinkX xx = new LinkX();
         xx.insert(1, 1);
         xx.insert(2, 2);
         xx.insert(3, 3);
+
+        xx.delete();
         xx.display();
+
     }
 
 }
@@ -23,25 +28,26 @@ class Node {
     public int id;
     public double value;
 
-
     public Node(int id, double value) {
         this.id = id;
         this.value = value;
     }
 
-    public Node() {
-
-    }
 }
 
 class LinkX {
 
-    private Node first = new Node();
+    private Node first;
 
     public void insert(int id, double value) {
         Node x = new Node(id, value);
         x.next = first;
         first = x;
+    }
+
+    public void delete() {
+        first = first.next;
+        System.out.println("成功删除1个元素");
     }
 
     public void display() {
