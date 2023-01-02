7f689cd896123ea5cfb53d82a77a51d3be4178da
zhangyu <zhangyu121.ok@163.com>
2023-01-02 10:41:28 +0800

    有序的list练习

--- demo/src/main/java/com/example/demo/pingshi/book/b1/c5/Link.java
+++ demo/src/main/java/com/example/demo/pingshi/book/b1/c5/Link.java
@@ -1,88 +1,60 @@
 package com.example.demo.pingshi.book.b1.c5;
 
 class Link {
-    public long dData;                  // data item
-    public Link next;                   // next link in list
 
-    // -------------------------------------------------------------
-    public Link(long dd)                // constructor
-    {
-        dData = dd;
+    public int data;
+    public Link next;
+
+    public Link(int data) {
+        this.data = data;
     }
 
-    // -------------------------------------------------------------
-    public void displayLink()           // display this link
-    {
-        System.out.print(dData + " ");
-    }
-}  // end class Link
+}
 
 class SortedList {
-    private Link first;                 // ref to first item
 
-    // -------------------------------------------------------------
-    public SortedList()                 // constructor
-    {
+    private Link first;
+
+    public SortedList() {
         first = null;
     }
 
-    // -------------------------------------------------------------
-    public boolean isEmpty()            // true if no links
-    {
-        return (first == null);
-    }
-
-    // -------------------------------------------------------------
-    public void insert(long key)        // insert, in order
-    {
-        Link newLink = new Link(key);    // make new link
-        Link previous = null;            // start at first
+    public void insert(int key) {
+        Link newLink = new Link(key);
+        Link previous = null;
         Link current = first;
-        // until end of list,
-        while (current != null && key > current.dData) {                             // or key > current,
+
+        while (current != null && key > current.data) {
             previous = current;
-            current = current.next;       // go to next item
+            current = current.next;
         }
-        if (previous == null)               // at beginning of list
-            first = newLink;              // first --> newLink
-        else                             // not at beginning
-            previous.next = newLink;      // old prev --> newLink
-        newLink.next = current;          // newLink --> old currnt
-    }  // end insert()
 
-    // -------------------------------------------------------------
-    public Link remove()           // return & delete first link
-    {                           // (assumes non-empty list)
-        Link temp = first;               // save first
-        first = first.next;              // delete first
-        return temp;                     // return value
+        if (previous == null) {
+            first = newLink;
+        } else {
+            previous.next = newLink;
+        }
+        newLink.next = current;
     }
 
-    // -------------------------------------------------------------
     public void displayList() {
-        System.out.print("List (first-->last): ");
-        Link current = first;       // start at beginning of list
-        while (current != null)      // until end of list,
-        {
-            current.displayLink();   // print data
-            current = current.next;  // move to next link
+        Link current = first;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
         }
-        System.out.println("");
     }
-}  // end class SortedList
+}
 
 class SortedListApp {
-    public static void main(String[] args) {                            // create new list
+    public static void main(String[] args) {
         SortedList theSortedList = new SortedList();
 
-        theSortedList.insert(20);    // insert 3 more items
-        theSortedList.insert(40);
+        theSortedList.insert(20);
+        theSortedList.insert(140);
         theSortedList.insert(60);
 
-        theSortedList.displayList(); // display list
+        theSortedList.displayList();
 
-        theSortedList.remove();      // remove an item
-
-        theSortedList.displayList(); // display list
-    }  // end main()
-}  // end class SortedListApp
+    }
+}
