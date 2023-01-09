29cf675df56236a210f52a7ec60e334e1abc2a40
zhangyu <zhangyu121.ok@163.com>
2023-01-09 17:18:37 +0800

    改double为int

--- demo/src/main/java/com/example/demo/pingshi/mongo/relproject/data/BB.java
+++ demo/src/main/java/com/example/demo/pingshi/mongo/relproject/data/BB.java
@@ -2,22 +2,16 @@ package com.example.demo.pingshi.mongo.relproject.data;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.bson.Document;
 import org.bson.UuidRepresentation;
 import org.bson.internal.UuidHelper;
 import org.bson.types.Binary;
+import org.junit.Test;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -28,190 +22,66 @@ import java.util.UUID;
 @Slf4j
 public class BB {
 
-    static MongoClient client = new MongoClient("localhost:27017");
-    public static MongoDatabase iotdb = client.getDatabase("Foo");
-
-    private Workbook wb;
-    private Sheet sheet;
-    private Row row;
-
-    public BB(String filepath) {
-        if (filepath == null) {
-            return;
-        }
-        String ext = filepath.substring(filepath.lastIndexOf("."));
-        try {
-            InputStream is = new FileInputStream(filepath);
-            if (".xls".equals(ext)) {
-                wb = new HSSFWorkbook(is);
-            } else if (".xlsx".equals(ext)) {
-                wb = new XSSFWorkbook(is);
-            } else {
-                wb = null;
-            }
-        } catch (FileNotFoundException e) {
-            log.error("FileNotFoundException", e);
-        } catch (IOException e) {
-            log.error("IOException", e);
-        }
-    }
-
-    /**
-     * 读取Excel表格表头的内容
-     *
-     * @param
-     * @return String 表头内容的数组
-     * @author zengwendong
-     */
-    public String[] readExcelTitle() throws Exception {
-        if (wb == null) {
-            throw new Exception("Workbook对象为空！");
-        }
-        sheet = wb.getSheetAt(0);
-        row = sheet.getRow(0);
-        // 标题总列数
-        int colNum = row.getPhysicalNumberOfCells();
-        System.out.println("colNum:" + colNum);
-        String[] title = new String[colNum];
-        for (int i = 0; i < colNum; i++) {
-            // title[i] = getStringCellValue(row.getCell((short) i));
-            title[i] = row.getCell(i).getCellFormula();
-        }
-        return title;
-    }
-
-    /**
-     * 读取Excel数据内容
-     *
-     * @param
-     * @return Map 包含单元格数据内容的Map对象
-     * @author zengwendong
-     */
-    public Map<Integer, Map<Integer, Object>> readExcelContent() throws Exception {
-        if (wb == null) {
-            throw new Exception("Workbook对象为空！");
-        }
-        Map<Integer, Map<Integer, Object>> content = new HashMap<Integer, Map<Integer, Object>>();
-        //sheet号
-        sheet = wb.getSheetAt(0);
-        // 得到总行数
-        int rowNum = 29;
-        System.out.println("行数:" + rowNum);
-        row = sheet.getRow(0);
-        int colNum = 2;
-        System.out.println("列数:" + colNum);
-        // 正文内容应该从第二行开始,第一行为表头的标题
-        for (int i = 1; i <= rowNum; i++) {
-            row = sheet.getRow(i);
-            int j = 0;
-            Map<Integer, Object> cellValue = new HashMap<Integer, Object>();
-            while (j < colNum) {
-                Object obj = getCellFormatValue(row.getCell(j));
-                cellValue.put(j, obj);
-                j++;
-            }
-            content.put(i, cellValue);
-        }
-        return content;
-    }
-
-    /**
-     * 根据Cell类型设置数据
-     *
-     * @param cell
-     * @return
-     * @author zengwendong
-     */
-    private Object getCellFormatValue(Cell cell) {
-        Object cellvalue = "";
-        if (cell != null) {
-            // 判断当前Cell的Type
-            switch (cell.getCellType()) {
-                case Cell.CELL_TYPE_NUMERIC:// 如果当前Cell的Type为NUMERIC
-                case Cell.CELL_TYPE_FORMULA: {
-                    // 判断当前的cell是否为Date
-                    if (DateUtil.isCellDateFormatted(cell)) {
-                        // 如果是Date类型则，转化为Data格式
-                        // data格式是带时分秒的：2013-7-10 0:00:00
-                        // cellvalue = cell.getDateCellValue().toLocaleString();
-                        // data格式是不带带时分秒的：2013-7-10
-                        Date date = cell.getDateCellValue();
-                        cellvalue = date;
-                    } else {// 如果是纯数字
-
-                        // 取得当前Cell的数值
-                        cellvalue = String.valueOf(cell.getNumericCellValue());
-                    }
-                    break;
-                }
-                case Cell.CELL_TYPE_STRING:// 如果当前Cell的Type为STRING
-                    // 取得当前的Cell字符串
-                    cellvalue = cell.getRichStringCellValue().getString();
-                    break;
-                default:// 默认的Cell值
-                    cellvalue = "";
-            }
-        } else {
-            cellvalue = "";
-        }
-        return cellvalue;
-    }
-
+    @SneakyThrows
     public static void main(String[] args) {
-        try {
-            String filepath = "/Users/zhangyu/Desktop/zhi/副本三坐标铸件数据_0103_蜡件.xlsx";
-            BB excelReader = new BB(filepath);
-            // 对读取Excel表格标题测试
-//			String[] title = excelReader.readExcelTitle();
-//			System.out.println("获得Excel表格的标题:");
-//			for (String s : title) {
-//				System.out.print(s + " ");
-//			}
 
-            // 对读取Excel表格内容测试
-            Map<Integer, Map<Integer, Object>> map = excelReader.readExcelContent();
-            System.out.println("获得Excel表格的内容:");
-            for (int i = 21; i <= map.size(); i++) {
-                System.out.println(map.get(i));
-                //insert data
-                Document doc = new Document();
-                Binary bb = new Binary((byte) 3, UuidHelper.encodeUuidToBinary(UUID.randomUUID(), UuidRepresentation.JAVA_LEGACY));
-                doc.put("_id", bb);
-                //taskNumber
-                doc.put("taskNumber", "22-11-11");
-                //batchNumberDown
-                doc.put("batchNumberDown", "22-11-11");
-                //partName
-                doc.put("partName", "22-11-11");
-                //partNumber
-                doc.put("partNumber", "22-11-11");
-                //batchType
-                doc.put("batchType", "22-11-11");
+        MongoClient client = new MongoClient("localhost:27017");
+        MongoDatabase iotdb = client.getDatabase("Foo");
+
+        //insert data
+        Document doc = new Document();
+        Binary bb = new Binary((byte) 3, UuidHelper.encodeUuidToBinary(UUID.randomUUID(), UuidRepresentation.JAVA_LEGACY));
+        doc.put("_id", bb);
+        //taskNumber
+        doc.put("taskNumber", "22-11-11");
+        //batchNumberDown
+        doc.put("batchNumberDown", "22-11-11");
+        //partName
+        doc.put("partName", "斜支板承力框架_铸件");
+        //partNumber
+        doc.put("partNumber", "S10.41.1001G-Z");
+        //batchType
+        doc.put("batchType", 1);
+
+        iotdb.getCollection("test").insertOne(doc);
+
+    }
 
 
-                //name
-                doc.put("name", map.get(i).get(0));
-                //type
-                doc.put("type", 1);
-                String name = map.get(i).get(0).toString();
-                int code = 0;
-                doc.put("code", code);
-                //value
-                doc.put("value", Double.parseDouble(map.get(i).get(1).toString()));
+    //改double为int
+    @Test
+    public void cc() {
 
-                //sn
-                if (name.contains("A") || name.contains("S")) {
-                    doc.put("sn", Integer.parseInt(name.substring(0, 1)));
-                }
-                System.out.println("zz:" + doc.toJson());
-                //iotdb.getCollection("cmm").insertOne(doc);
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("未找到指定路径的文件!");
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        MongoClient client = new MongoClient("localhost:27017");
+        MongoDatabase iotdb = client.getDatabase("Foo");
+        List<Document> list = iotdb.getCollection("task").find().into(new ArrayList<>());
+
+        list.forEach(e -> {
+            Binary bb = new Binary((byte) 3, UuidHelper.encodeUuidToBinary(UUID.fromString(e.get("_id").toString()), UuidRepresentation.JAVA_LEGACY));
+            Document query = new Document();
+            query.put("_id", bb);
+            Document doc = new Document();
+            Document set = new Document();
+            Double batchType = e.getDouble("batchType");
+            set.put("batchType", batchType.intValue());
+            doc.put("$set", set);
+            iotdb.getCollection("task").updateOne(query, doc);
+        });
+       /* //insert data
+        Document doc = new Document();
+        Binary bb = new Binary((byte) 3, UuidHelper.encodeUuidToBinary(UUID.randomUUID(), UuidRepresentation.JAVA_LEGACY));
+        doc.put("_id", bb);
+        //taskNumber
+        doc.put("taskNumber", "22-11-11");
+        //batchNumberDown
+        doc.put("batchNumberDown", "22-11-11");
+        //partName
+        doc.put("partName", "斜支板承力框架_铸件");
+        //partNumber
+        doc.put("partNumber", "S10.41.1001G-Z");
+        //batchType
+        doc.put("batchType", 1);*/
+
     }
 
 }
