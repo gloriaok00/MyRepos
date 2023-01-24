bff1d480fb9139c6d3f57ba53841cafdd7ec8a54
zhangyu <zhangyu121.ok@163.com>
2023-01-24 17:04:03 +0800

    FutureTask再来

--- demo/src/main/java/com/example/demo/mianshi/thread/t12/CallerTask.java
+++ demo/src/main/java/com/example/demo/mianshi/thread/t12/CallerTask.java
@@ -1,7 +1,8 @@
 package com.example.demo.mianshi.thread.t12;
 
+import lombok.SneakyThrows;
+
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 //创建任务类，类似Runable
@@ -9,20 +10,13 @@ public class CallerTask implements Callable<String> {
 
     @Override
     public String call() throws Exception {
-        return "hello";
+        return "Hello";
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        // 创建异步任务
-        FutureTask<String> futureTask = new FutureTask<>(new CallerTask());
-        //启动线程
-        new Thread(futureTask).start();
-        try {
-            //等待任务执行完毕，并返回结果
-            String result = futureTask.get();
-            System.out.println(result);
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
+    @SneakyThrows
+    public static void main(String[] args) {
+        FutureTask<String> task = new FutureTask<>(new CallerTask());
+        task.run();
+        System.out.println(task.get());
     }
 }
