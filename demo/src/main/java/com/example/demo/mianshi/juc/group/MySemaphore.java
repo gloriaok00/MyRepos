e328c291a2527f54ac05ada79921e19bc66888e6
zhangyu <zhangyu121.ok@163.com>
2023-01-31 13:20:12 +0800

    信号量学习

--- demo/src/main/java/com/example/demo/mianshi/juc/group/MySemaphore.java
+++ demo/src/main/java/com/example/demo/mianshi/juc/group/MySemaphore.java
@@ -4,22 +4,22 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @author zhangyu
- * @date 2019-09-14 16:22
- * @descprition 3个车位，6辆车
+ *  @description 6辆车抢3个车位
+ *  @date 2019-09-14 16:22
+ *  @date 2023/1/31 13:16
  */
 public class MySemaphore {
 
     public static void main(String[] args) {
         Semaphore semaphore = new Semaphore(3);
-        for (int i = 1; i < 6; i++) {
+        for (int i = 1; i <= 6; i++) {
             final int temp = i;
             new Thread(() -> {
                 try {
                     semaphore.acquire();
                     System.out.println("第" + temp + "个车占了车位");
                     TimeUnit.MILLISECONDS.sleep(3);
-                    System.out.println("第" + temp + "个车停车3秒钟结束");
+                    System.out.println("第" + temp + "个车停车3豪钟结束");
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
