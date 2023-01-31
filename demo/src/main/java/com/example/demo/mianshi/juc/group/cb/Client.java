cc3e6ec1bbbf3a06676067f35c04d3bfafe0329e
zhangyu <zhangyu121.ok@163.com>
2023-01-31 14:51:48 +0800

    CyclicBarrier练习

---
+++ demo/src/main/java/com/example/demo/mianshi/juc/group/cb/Client.java
@@ -0,0 +1,23 @@
+package com.example.demo.mianshi.juc.group.cb;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * @description CyclicBarrier练习
+ * @date 2023/1/31 14:33
+ */
+public class Client {
+
+    public static void main(String[] args) throws Exception {
+
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3, new TourGuideTask());
+        Executor executor = Executors.newFixedThreadPool(3);
+        //登哥最大牌，到的最晚
+        executor.execute(new TravelTask(cyclicBarrier, "哈登", 5));
+        executor.execute(new TravelTask(cyclicBarrier, "保罗", 3));
+        executor.execute(new TravelTask(cyclicBarrier, "戈登", 1));
+
+    }
+}
