6886eccf67fc78cccea3896e8ee552cf1f4c40d9
zhangyu <zhangyu121.ok@163.com>
2023-02-02 06:55:57 +0800

    singlePool搭配的也是无界阻塞队列

---
+++ demo/src/main/java/com/example/demo/pingshi/mythread/t15/SinglePool.java
@@ -0,0 +1,37 @@
+package com.example.demo.pingshi.mythread.t15;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @description singlePool搭配的也是无界阻塞队列
+ * @date 2023/2/2 06:53
+ */
+public class SinglePool {
+
+    public static void main(String[] args) {
+        ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+
+        for (int i = 0; i < 5; i++) {
+            final int index = i;
+            singleThreadExecutor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        SimpleDateFormat sdf = new SimpleDateFormat(
+                                "HH:mm:ss");
+                        System.out.println("运行时间: " +
+                                sdf.format(new Date()) + " " + index);
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        singleThreadExecutor.shutdown();
+    }
+}
