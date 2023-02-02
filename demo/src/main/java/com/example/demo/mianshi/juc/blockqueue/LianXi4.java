0e9c2c9a36afd93523a5724dacc49b378381f647
zhangyu <zhangyu121.ok@163.com>
2023-02-02 21:43:49 +0800

    随机练习

---
+++ demo/src/main/java/com/example/demo/mianshi/juc/blockqueue/LianXi4.java
@@ -0,0 +1,35 @@
+package com.example.demo.mianshi.juc.blockqueue;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class LianXi4 {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        for (int i = 1; i <= 20; i++) {
+            executorService.execute(new MyRunnable(i));
+        }
+    }
+}
+
+@Slf4j
+class MyRunnable implements Runnable {
+    int id;
+
+    MyRunnable(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(3000);
+            System.out.println("threadPool task id:" + id);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
