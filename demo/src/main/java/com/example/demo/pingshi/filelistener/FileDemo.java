398c42b51eb30f7e43882abece359fb203c34ac8
zhangyu <zhangyu121.ok@163.com>
2023-02-02 09:37:59 +0800

    文件监控

---
+++ demo/src/main/java/com/example/demo/pingshi/filelistener/FileDemo.java
@@ -0,0 +1,41 @@
+package com.example.demo.pingshi.filelistener;
+
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.commons.io.filefilter.HiddenFileFilter;
+import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.io.monitor.FileAlterationMonitor;
+import org.apache.commons.io.monitor.FileAlterationObserver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @description 文件监控
+ * @date 2023/2/2 09:27
+ */
+
+public class FileDemo {
+    public static void main(String[] args) throws Exception{
+        // 监控目录
+        String rootDir = "/Users/zhangyu/Desktop/test";
+        // 轮询间隔 5 秒
+        long interval = TimeUnit.SECONDS.toMillis(1);
+        // 创建过滤器
+        IOFileFilter directories = FileFilterUtils.and(
+                FileFilterUtils.directoryFileFilter(),
+                HiddenFileFilter.VISIBLE);
+        IOFileFilter files       = FileFilterUtils.and(
+                FileFilterUtils.fileFileFilter(),
+                FileFilterUtils.suffixFileFilter(".txt"));
+        IOFileFilter filter = FileFilterUtils.or(directories, files);
+        // 使用过滤器
+        FileAlterationObserver observer = new FileAlterationObserver(new File(rootDir), filter);
+        //不使用过滤器
+        //FileAlterationObserver observer = new FileAlterationObserver(new File(rootDir));
+        observer.addListener(new FileListener());
+        //创建文件变化监听器
+        FileAlterationMonitor monitor = new FileAlterationMonitor(interval, observer);
+        // 开始监控
+        monitor.start();
+    }
+}
