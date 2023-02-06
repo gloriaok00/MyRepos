f1fe8faf4bc8e9d102a970db4fd19d5b469f25f1
zhangyu <zhangyu121.ok@163.com>
2023-02-07 06:11:07 +0800

    改旧东西

--- demo/src/main/java/com/example/demo/mianshi/juc/blockqueue/MyCallable.java
+++ demo/src/main/java/com/example/demo/mianshi/juc/blockqueue/MyCallable.java
@@ -1,30 +1,33 @@
 package com.example.demo.mianshi.juc.blockqueue;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
 
 /**
- * @author zhangyu
- * @date 2019-09-15 15:34
+ *  @description 改旧东西
+ *  @date  2019-09-15 15:34
+ *  @date  2023/2/7 06:10
  */
 
-class MyThread implements Callable {
+class MyThread implements Callable<Integer> {
     @Override
-    public Object call() throws Exception {
-        System.out.println(" 计算中 from callable");
-        TimeUnit.SECONDS.sleep(2);
+    public Integer call() throws Exception {
+        System.out.println("callable方法..");
         return 101;
     }
 }
 
 public class MyCallable {
+
     public static void main(String[] args) throws Exception {
-        FutureTask futureTask = new FutureTask(new MyThread());
-        new Thread(futureTask, "AA").start();
-        System.out.println(Thread.currentThread().getName() + "继续...");
+        FutureTask<Integer> futureTask = new FutureTask<>(new MyThread());
+        ExecutorService pool=Executors.newSingleThreadExecutor();
+        pool.submit(futureTask);
         int result2 = 100;
-        int result1 = (int) futureTask.get();
+        int result1 = futureTask.get();
         System.out.println(result1 + result2);
     }
+
 }
