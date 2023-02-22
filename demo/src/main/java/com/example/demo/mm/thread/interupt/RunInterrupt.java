90dbe4eb4d667d80c1159a16eef5e7c1ad3c59ce
zhangyu <zhangyu121.ok@163.com>
2023-02-22 10:36:16 +0800

    线程异常学习

---
+++ demo/src/main/java/com/example/demo/mm/thread/interupt/RunInterrupt.java
@@ -0,0 +1,29 @@
+package com.example.demo.mm.thread.interupt;
+
+/**
+ * @description 运行状态下 调interrupt是没用的
+ * @date 2023/2/22 09:48
+ */
+
+public class RunInterrupt {
+
+    public static void main(String[] args) {
+
+        Thread xx= new ThreadDemo();
+        xx.start();
+        xx.interrupt();
+        while (true){
+            System.out.println(2);
+        }
+
+    }
+}
+
+class ThreadDemo extends Thread{
+    @Override
+    public void run() {
+        while(true){
+            System.out.println("1");
+        }
+    }
+}
