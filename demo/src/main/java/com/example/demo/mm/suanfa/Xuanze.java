81f5d19d50ed59a6e894a8f8dc15fb072f3dd47a
zhangyu <zhangyu121.ok@163.com>
2023-04-16 12:58:25 +0800

    选择排序

--- demo/src/main/java/com/example/demo/mm/suanfa/Xuanze.java
+++ demo/src/main/java/com/example/demo/mm/suanfa/Xuanze.java
@@ -6,20 +6,20 @@ package com.example.demo.mm.suanfa;
  */
 
 public class Xuanze {
-
+    
     public static void main(String[] args) {
         int[] a = {13, 22, 130, 4, 51, 69};
-        for (int i = 0; i < a.length - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < a.length; j++) {
-                if (a[j] < a[minIndex]) {
-                    minIndex = j;
-                }
+        for (int i = 0; i < a.length-1; i++) {
+            int min=i;
+            for (int j = i+1; j < a.length; j++) {
+                 if(a[j]<a[min]){
+                      min=j;
+                 }
             }
-            if (minIndex != i) {
-                int temp = a[i];
-                a[i] = a[minIndex];
-                a[minIndex] = temp;
+            if(min!=i){
+                int temp=a[i];
+                a[i]=a[min];
+                a[min]=temp;
             }
         }
         for (int i = 0; i < a.length; i++) {
