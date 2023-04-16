3d3a0f302326410a8eb32a9172f785e19e42464a
zhangyu <zhangyu121.ok@163.com>
2023-04-16 10:15:01 +0800

    冒泡排序

---
+++ demo/src/main/java/com/example/demo/mm/suanfa/Maopao.java
@@ -0,0 +1,26 @@
+package com.example.demo.mm.suanfa;
+
+/**
+ * @description
+ * @date 2023/4/16 10:00
+ */
+
+public class Maopao {
+
+    public static void main(String[] args) {
+        int[] a = {13, 22, 130, 4, 51, 69};
+        int temp;
+        for (int i = 0; i < a.length-1; i++) {
+            for (int j = 0; j < a.length-i-1; j++) {
+                if (a[j] > a[j + 1]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
